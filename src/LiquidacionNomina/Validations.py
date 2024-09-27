4f88b0a54149ee68c73d11076974ddf6d12ad155
sdoh2745 <142015989+sdoh2745@users.noreply.github.com>
2024-09-27 08:35:23 -0500

    Update Validations.py

--- src/LiquidacionNomina/Validations.py
+++ src/LiquidacionNomina/Validations.py
@@ -18,30 +18,33 @@ class MoreThan8HoursWorkedOnHoliday(Exception):
 class ZeroWeeksWorked(Exception):
     pass
 
+class ZeroSalary(Exception):
+    pass
+
 def validate_and_convert(entry, variable_name, should_be_integer=False):
     try:
         entry_str = str(entry)
         
         # Check if the entry contains a comma as a decimal separator
         if ',' in entry_str:
-            raise CommaSeparator(f"ERROR: Invalid data in {variable_name}\n Use a period (.) as a decimal separator, not a comma (,).")
+            raise CommaSeparator(f"ERROR: Dato inválido en {variable_name}: Use un punto (.) como separador decimal, no una coma (,)")
 
         # Convert to float to ensure it's numeric
         value = float(entry)
 
         # If it should be an integer, check that it has no decimals
         if should_be_integer and value != int(value):
-            raise NotAnIntegerValue(f"ERROR: Invalid data in {variable_name}\n An integer value was expected.")
+            raise NotAnIntegerValue(f"ERROR: Dato inválido en {variable_name}: Se esperaba un número entero.")
 
         # Check if it's negative
         if value < 0:
-            raise NegativeValue(f"ERROR: Invalid data in {variable_name}\n Numbers cannot be negative.")
+            raise NegativeValue(f"ERROR: Dato inválido en {variable_name}: Los números no pueden ser negativos.")
 
         # Return the correct type (integer or float)
         return int(value) if should_be_integer else value
 
     except ValueError:
-        raise InvalidValue(f"ERROR: Invalid data in {variable_name}: Ensure it is a numeric value.\n not negative and without letters or special characters.")
+        raise InvalidValue(f"ERROR: Dato inválido en {variable_name}: Asegúrese de que sea un valor númerico, no negativo y sin letras o caracteres especiales.")
 
 def validate_variables(variables):
     for variable_name in variables:
